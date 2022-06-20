5258ba46ea6d26a1d194b601b05c6961e3ddd46f
vigil-senpai <vincent.pradipta@binus.ac.id>
2022-06-20 14:58:50 +0700

    error when qty below 1

--- middleware/product-qty-validator.js
+++ middleware/product-qty-validator.js
@@ -8,6 +8,9 @@ module.exports  = async(req, res, next) => {
     const getQtyQuery = knex('MsProduct').select('stocks').where({productID: productID})
     const result = await queryPromise(getQtyQuery)
     const productQty = result[0].stocks
+    if(quantity < 1) {
+        throw new BadRequestError('Quantity is invalid (can\'t be less than 1)')
+    }
     if(productQty < quantity) {
         throw new BadRequestError('Quantity is invalid (more than stocks)')
     }
